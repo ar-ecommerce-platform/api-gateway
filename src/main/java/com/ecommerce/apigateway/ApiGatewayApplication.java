d7c188a8ddef5b57a5296d1a048f0c7f415ac113
Alexis Rodriguez <alexisrodriguezdev@gmail.com>
2025-10-28 18:36:02 -0500

    chore: fix guava error

--- src/main/java/com/ecommerce/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/ecommerce/apigateway/ApiGatewayApplication.java
@@ -1,8 +1,9 @@
-package com.ecommerce.api_gateway;
+package com.ecommerce.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** Entry point for the API Gateway application. */
 @SpringBootApplication
 public class ApiGatewayApplication {
 
